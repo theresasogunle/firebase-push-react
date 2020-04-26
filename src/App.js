11434336a627cc80d6eb71b97127ad2ace0d668b
Omowunmi <sogunledolapo@gmail.com>
2020-04-26 10:06:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { askForPermissioToReceiveNotifications } from './push-notification';
+
 
 function App() {
+
+  React.useEffect(()=>{
+    askForPermissioToReceiveNotifications()
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
